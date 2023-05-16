eb600475f1b530d433771e9ee69a6a2e70adc376
Lucas Kappel <81535666+lucaskappel@users.noreply.github.com>
2023-05-16 16:36:36 -0500

    Trasnformations, Fills, and Debugging
    Improved debugging loop in Engine.cs
    Created basic gamestate tracking in engine
    Added rotations and mirrors trasnformations to Bitboard.cs
    Added north and south fill algorithms to bitboard
    added clear to bitboard

--- Bitboard.cs
+++ Bitboard.cs
@@ -6,25 +6,29 @@ namespace ZBC {
 		
 		// vars //
 		
-		/* Compass Rose
-			nw      n     ne
-			   +7  +8  +9
-                 \  |  /
-			w -1    0    +1 e
-                 /  |  \
-               -9  -8  -7
-			sw      s     se
-		*/
+/* Compass Rose
+
+nw    n    ne
+  +7  +8  +9
+    \  |  /
+w  -1  0  +1  e
+    /  |  \
+  -9  -8  -7
+sw    s    se
+
+*/
 		
-		private static char[] piece_symbol = {'p', 'n', 'b', 'r', 'q', 'k'};
-		private static string[] piece_name = {"pawn", "knight", "bishop", "rook", "queen", "king"};
+		private static char[] piece_symbol = {'W', 'b', 'p', 'n', 'b', 'r', 'q', 'k'};
+		private static string[] piece_name = {"White", "black", "pawn", "knight", "bishop", "rook", "queen", "king"};
+		private static string[] compass_rose_index_book = {"n", "ne", "e", "se", "s", "sw", "w", "nw"};
+		private static int[] compass_rose = {8, 9, 1, -7, -8, -9, -1, 7};
 		
 		// piece_deltas
 		
 		public ulong[] pieces;
 		
-		public Bitboard(string serialization_method = ""){
-			this.pieces = new ulong[] { // color & piece ; ulongs are 64 bits, 1 bit per square. This represents piece positions.
+		public Bitboard(){
+			this.pieces = new ulong[] { // little-endian rank-file mapping
 				0xFFFF000000000000, // white
 				0x000000000000FFFF, // black
 				0x00FF00000000FF00, // pawn
@@ -81,7 +85,118 @@ namespace ZBC {
 				string_iterator--;
 			}
 			return " " + String.Join(" ", return_string.ToCharArray());
-		}
+		} // end static DebugBitmap
+		
+		// fills
+		
+		public static ulong Fill(ulong bitmap, string direction){
+			int compass_rose_index = Array.IndexOf(compass_rose_index_book, direction);
+			if(compass_rose_index < 0){ 
+				Console.WriteLine("compass rose direction was invalid: " + direction);
+				return 0x0; 
+			}
+			int direction_shift = compass_rose[compass_rose_index];
+			Console.WriteLine(DebugBitmap(bitmap));
+			bitmap |= (bitmap << direction_shift * 1);
+			Console.WriteLine(DebugBitmap(bitmap));
+			bitmap |= (bitmap << direction_shift * 2);
+			Console.WriteLine(DebugBitmap(bitmap));
+			bitmap |= (bitmap << direction_shift * 4);
+			Console.WriteLine(DebugBitmap(bitmap));
+			return bitmap;
+		} // end static Fill
+		
+		public static ulong FillNorth(ulong bitmap){
+			bitmap |= (bitmap <<  8);
+			bitmap |= (bitmap << 16);
+			bitmap |= (bitmap << 32);
+			return bitmap;
+		} // end static FillNorth
+		
+		public static ulong FillSouth(ulong bitmap){
+			bitmap |= (bitmap >>  8);
+			bitmap |= (bitmap >> 16);
+			bitmap |= (bitmap >> 32);
+			return bitmap;
+		} // end static FillSouth
+		
+		// transforms (3 digit number is a reference to degrees)
+		
+		public static ulong TransformMirror000(ulong bitmap){
+			/* 
+			mirror bitmap about the center ranks, along the X axis
+			Parallel prefix approach
+			reverses all eight bytes
+			performs little <-> big endian transformation
+			*/
+			const ulong k1 = 0x00FF00FF00FF00FF;
+			const ulong k2 = 0x0000FFFF0000FFFF;
+			bitmap = ( (bitmap >> 8 ) & k1) | ( (bitmap & k1) << 8 );
+			bitmap = ( (bitmap >> 16) & k2) | ( (bitmap & k2) << 16);
+			bitmap = ( (bitmap >> 32)	  )	| ( (bitmap     ) << 32);
+			return bitmap;
+		} // end static TransformMirror000
+		
+		public static ulong TransformMirror090(ulong bitmap){
+			/* 
+			mirror bitmap about the center files, along the Y axis
+			parallel prefix approach
+			reverses the bits of each byte
+			*/
+			const ulong k1 = 0x5555555555555555;
+			const ulong k2 = 0x3333333333333333;
+			const ulong k4 = 0x0f0f0f0f0f0f0f0f;
+			bitmap = ( ( bitmap >> 1) & k1) | ( (bitmap & k1) << 1);
+			bitmap = ( ( bitmap >> 2) & k2) | ( (bitmap & k2) << 2);
+			bitmap = ( ( bitmap >> 4) & k4) | ( (bitmap & k4) << 4);
+			return bitmap;
+		} // end static TransformMirror090
+		
+		public static ulong TransformMirror045(ulong bitmap){
+			/* 
+			mirror bitmap the diagnonal (45 degree axis), a1<->h8
+			*/
+			ulong temp;
+			const ulong k1 = 0x5500550055005500;
+			const ulong k2 = 0x3333000033330000;
+			const ulong k4 = 0x0f0f0f0f00000000;
+			temp    = k4 & (bitmap ^ (bitmap << 28) );
+			bitmap ^=      (temp   ^ (temp   >> 28) );
+			temp    = k2 & (bitmap ^ (bitmap << 14) );
+			bitmap ^=      (temp   ^ (temp   >> 14) );
+			temp    = k1 & (bitmap ^ (bitmap <<  7) );
+			bitmap ^=      (temp   ^ (temp   >>  7) );
+			return bitmap;
+		} // end static TransformMirror045
+		
+		public static ulong TransformMirror135(ulong bitmap){
+			/* 
+			mirror bitmap the anti-diagnonal (135 degree axis), a8<->h1
+			*/
+			ulong temp;
+			const ulong k1 = 0xaa00aa00aa00aa00;
+			const ulong k2 = 0xcccc0000cccc0000;
+			const ulong k4 = 0x0f0f0f0f0f0f0f0f;
+			temp    =      (bitmap ^ (bitmap << 36) );
+			bitmap ^= k4 & (  temp ^ (bitmap >> 36) );
+			temp    = k2 & (bitmap ^ (bitmap << 18) );
+			bitmap ^=      (temp   ^ (temp   >> 18) );
+			temp    = k1 & (bitmap ^ (bitmap <<  9) );
+			bitmap ^=      (temp   ^ (temp   >>  9) );
+			return bitmap;
+		} // end static TransformMirror135
+		
+		public static ulong TransformRotate180(ulong bitmap){
+			return TransformMirror090( TransformMirror000(bitmap) );
+		} // end static TransformRotate180
+		
+		public static ulong TransformRotate270(ulong bitmap){
+			return TransformMirror000( TransformMirror045(bitmap) );
+		} // end static TransformRotate270
+		
+		public static ulong TransformRotate090(ulong bitmap){
+			return TransformMirror045( TransformMirror000(bitmap) );
+		} // end static TransformRotate090
 		
 		// methods//
 		
@@ -117,7 +232,14 @@ namespace ZBC {
 			}
 			return;
 		} // end PieceMove
-		
+	
+		public void ClearBoard(){
+			int piece_iterator = 0;
+			while(piece_iterator < this.pieces.Length){
+				this.pieces[piece_iterator] = 0x0;
+			}
+		} // end ClearBoard
+	
 		public Bitboard Copy(){
 			Bitboard return_bitboard = new Bitboard();
 			return_bitboard.pieces = new ulong[this.pieces.Length];
@@ -128,7 +250,7 @@ namespace ZBC {
 				piece_iterator++;
 			}
 			return return_bitboard;
-		} // end create_copy
+		} // end Copy()
 		
 		public override string ToString(){
 			char[] return_bitboard = UlongToString(0x0).ToCharArray();
@@ -143,7 +265,7 @@ namespace ZBC {
 					char[] current_bitmap = UlongToString( this.pieces[piece_iterator] & this.pieces[color_iterator] ).ToCharArray();
 					
 					// Determine the piece symbol. piece_iterator determins the character, color_iterator determines case.
-					char current_piece_symbol = Bitboard.piece_symbol[piece_iterator - 2];
+					char current_piece_symbol = Bitboard.piece_symbol[piece_iterator];
 					if(color_iterator == 0){ 
 						current_piece_symbol = Char.ToUpper(current_piece_symbol); 
 					}
--- Engine.cs
+++ Engine.cs
@@ -1,31 +1,49 @@
 using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace ZBC{
     public class Engine{
 		
         public static void Main(string[] args){
 			
-			Bitboard a = new Bitboard();
-			Console.WriteLine(a.ToString());
+			List<Bitboard> history = new List<Bitboard>();
+			history.Add(new Bitboard());
+			
 			
 			while(true){
+				Console.WriteLine(history.ToString());
+				Console.WriteLine(history.Count);
+				Console.WriteLine(history[history.Count - 1].ToString());
+				
 				string[] input = Console.ReadLine().Split(' ');
 				int color = 0;
 				int piece = 2;
 				if(input[0].Equals("create") && Int32.TryParse(input[1], out color) && Int32.TryParse(input[2], out piece) ){
-					a.PieceCreate(new int[] {color, piece}, Bitboard.CoordinateToUlong(input[3]));
+					history.Add(history[history.Count - 1].Copy());
+					history[history.Count - 1].PieceCreate(new int[] {color, piece}, Bitboard.CoordinateToUlong(input[3]));
 				}
 				else if(input[0].Equals("remove")){
-					a.PieceRemove(Bitboard.CoordinateToUlong(input[1]));
+					history.Add(history[history.Count - 1].Copy());
+					history[history.Count - 1].PieceRemove(Bitboard.CoordinateToUlong(input[1]));
 				}
 				else if(input[0].Equals("move")){
-					a.PieceMove(Bitboard.CoordinateToUlong(input[1]), Bitboard.CoordinateToUlong(input[2]));
+					history.Add(history[history.Count - 1].Copy());
+					history[history.Count - 1].PieceMove(Bitboard.CoordinateToUlong(input[1]), Bitboard.CoordinateToUlong(input[2]));
 				}
 				else if(input[0].Equals("dbc")){
 					Console.WriteLine(Bitboard.CoordinateToUlong(input[1]));
 				}
-				Console.WriteLine(a.ToString());
+				else if(input[0].Equals("clear")){
+					history.Add(history[history.Count - 1].Copy());
+					history[history.Count - 1].ClearBoard();
+				}
+				else if(input[0].Equals("undo")){
+					history.RemoveAt(history.Count - 1);
+				}
+				else if(input[0].Equals("end")){
+					break;
+				}
 			}
 
         } // end method Main
