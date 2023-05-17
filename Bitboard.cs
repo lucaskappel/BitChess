ea15192cff0f864d57b3be242fe96821f1bd824b
Lucas Kappel <81535666+lucaskappel@users.noreply.github.com>
2023-05-17 11:35:17 -0500

    Reworking Mapping
    Realized I misunderstood some of the mapping technicalities. Reworking base structure to be correct. Also added indexing methods and improving debugging menu.

--- Bitboard.cs
+++ Bitboard.cs
@@ -2,51 +2,157 @@ using System;
 
 namespace ZBC {
 	
-	public class Bitboard{
+	public class Bitboard{ // All code designed for Little-Endian Rank-File Mapping
 		
 		// vars //
 		
-/* Compass Rose
-
-nw    n    ne
-  +7  +8  +9
-    \  |  /
-w  -1  0  +1  e
-    /  |  \
-  -9  -8  -7
-sw    s    se
-
-*/
+		public static string[] piece_name = {"white", "black", "pawn", "knight", "bishop", "rook", "queen", "king"};
+		private static char[] piece_symbol = {'w', 'b', 'p', 'n', 'b', 'r', 'q', 'k'};
 		
-		private static char[] piece_symbol = {'W', 'b', 'p', 'n', 'b', 'r', 'q', 'k'};
-		private static string[] piece_name = {"White", "black", "pawn", "knight", "bishop", "rook", "queen", "king"};
-		private static string[] compass_rose_index_book = {"n", "ne", "e", "se", "s", "sw", "w", "nw"};
+		public static char[] file_index = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+		public static char[] rank_index = {'1', '2', '3', '4', '5', '6', '7', '8'};
+		
+		private static string[] compass_rose_index = {"n", "ne", "e", "se", "s", "sw", "w", "nw"};
 		private static int[] compass_rose = {8, 9, 1, -7, -8, -9, -1, 7};
-		
-		// piece_deltas
+		/*
+			   nw    n   ne
+				+7  +8  +9
+			   	  \  |  /
+			  w  -1  0  +1  e
+				  /  |  \
+			    -9  -8  -7
+			   sw    s    se
+		*/
 		
 		public ulong[] pieces;
 		
 		public Bitboard(){
 			this.pieces = new ulong[] { // little-endian rank-file mapping
-				0xFFFF000000000000, // white
-				0x000000000000FFFF, // black
+				0x000000000000ffff, // white
+				0xffff000000000000, // black
 				0x00FF00000000FF00, // pawn
 				0x4200000000000042, // knight
 				0x2400000000000024, // bishop
 				0x8100000000000081, // rook
-				0x1000000000000010, // queen
-				0x0800000000000008, // king
+				0x0800000000000008, // queen
+				0x1000000000000010, // king
 			};
 		} // end constructor Bitboard
 		
+		// indexing //
+		
+		public static int SquareIndex(int fileIndex, int rankIndex){
+			/* translates the rank and file indicies to the square's bit index
+			a1 is the least significant bit at index 0 (2^0)
+			h8 is the most significant bit at index 63 (2^63)
+			
+			8  56 57 58 59 60 61 62 63
+			7  48 49 50 51 52 53 54 55
+			6  40 41 42 43 44 45 46 47
+			5  32 33 34 35 36 37 38 39
+			4  24 25 26 27 28 29 30 31
+			3  16 17 18 19 20 21 22 23
+			2  08 09 10 11 12 13 14 15
+			1  00 01 02 03 04 05 06 07
+			
+			   a  b  c  d  e  f  g  h
+			   
+			*/
+			if(fileIndex < 0 || rankIndex < 0){
+				Console.WriteLine("Invalid rank or file\nrank: " + rankIndex.ToString() + "\nfile: " + fileIndex.ToString());
+				return -1;
+			}
+			return 8 * rankIndex + fileIndex;
+		} // end static SquareIndex(int, int)
+		
+		public static int SquareIndex(string coordinate){
+			char[] coordinateArray;
+			try{ 
+				coordinateArray = coordinate.ToCharArray(); 
+			}
+			catch(Exception e){ 
+				Console.WriteLine(e.Message + "\nError trying to convert coordinate to SquareIndex: " + coordinate);
+				return -1;	
+			}
+			if(coordinateArray.Length != 2){ 
+				Console.WriteLine("Coordinate was not properly formatted: " + coordinate);
+				return -1; 
+			}
+			
+			int fileIndex = Array.IndexOf(file_index, coordinateArray[0]);
+			int rankIndex = Array.IndexOf(rank_index, coordinateArray[1]);
+			
+			return SquareIndex(fileIndex, rankIndex);
+		} // end static SquareIndex(string)
+		
+		public static int FileIndex(int squareIndex){
+			/* Translates the square index into the file index.
+			FileIndex = squareIndex % 8 = squareIndex & 8
+			
+			7 == 00000111
+			
+			a1 = 00000000 & 00000111 = 00000000 = 0
+			b2 = 00001001 & 00000111 = 00000001 = 1
+			c3 = 00010010 & 00000111 = 00000010 = 2
+			d4 = 00011011 & 00000111 = 00000011 = 3
+			e5 = 00100100 & 00000111 = 00000100 = 4
+			f6 = 00101101 & 00000111 = 00000101 = 5
+			g7 = 00110110 & 00000111 = 00000110 = 6
+			h8 = 00111111 & 00000111 = 00000111 = 7
+			*/
+			return squareIndex & 7;
+		}
+		
+		public static int RankIndex(int squareIndex){
+			/* Translates the square index into the rank index.
+			RankIndex = squareIndex div 8 = squareIndex >> 3
+			
+			a1 = 00000000 >> 3 = 00000000 = 0
+			b2 = 00001001 >> 3 = 00000001 = 1
+			c3 = 00010010 >> 3 = 00000010 = 2
+			d4 = 00011011 >> 3 = 00000011 = 3
+			e5 = 00100100 >> 3 = 00000100 = 4
+			f6 = 00101101 >> 3 = 00000101 = 5
+			g7 = 00110110 >> 3 = 00000110 = 6
+			h8 = 00111111 >> 3 = 00000111 = 7
+			*/
+			return squareIndex >> 3;
+		}
+
+		public static ulong SquareIndexToBitmap(int squareIndex){
+			return (ulong)Math.Pow(2, squareIndex);
+		}
+
 		// utilities//		
+		
+		public static char[] UlongToVisualizedChars(ulong bitmap){
+			// converts to char[64] and orders the chars to the bitboard indexing
+			
+			// Add leading zeroes until we get to 64 elements
+			string ulong_as_string = Convert.ToString(unchecked((long) bitmap), 2);
+			while(ulong_as_string.Length < 64){
+				ulong_as_string = "0" + ulong_as_string; 
+			}
+			
+			// Now map this to the visual chess board
+			string mappedString = "";
+			char[] workbench;
+			while(mappedString.Length < 64){
+				workbench = ulong_as_string.Substring(mappedString.Length, 8).ToCharArray();
+				Array.Reverse(workbench);
+				mappedString += new string(workbench);
+			}
+			return mappedString.ToCharArray();
+		}
 		
 		public static string UlongToString(ulong bitmap){
 			string ulong_as_string = Convert.ToString(unchecked((long) bitmap), 2);
 			while(ulong_as_string.Length < 64){ // Add leading zeroes until we get to 64 elements
 				ulong_as_string = "0" + ulong_as_string; 
 			}
+			
+			
+			
 			return ulong_as_string;
 		} // end UlongToString
 		
@@ -75,7 +181,7 @@ sw    s    se
 			return (ulong)0x1 << - rank_file[0] + (8 - rank_file[1]) * 8 - 1;
 			
 		} // end CoordinateToUlong
-		
+			
 		public static string DebugBitmap(ulong bitmap){
 			string return_string_base = UlongToString(bitmap);
 			string return_string = "";
@@ -90,18 +196,20 @@ sw    s    se
 		// fills
 		
 		public static ulong Fill(ulong bitmap, string direction){
-			int compass_rose_index = Array.IndexOf(compass_rose_index_book, direction);
-			if(compass_rose_index < 0){ 
+			int compassRoseIndex = Array.IndexOf(compass_rose_index, direction);
+			if(compassRoseIndex < 0){ 
 				Console.WriteLine("compass rose direction was invalid: " + direction);
 				return 0x0; 
 			}
-			int direction_shift = compass_rose[compass_rose_index];
+			
+			int direction_shift = compass_rose[compassRoseIndex];
+			Console.WriteLine(direction_shift);
 			Console.WriteLine(DebugBitmap(bitmap));
-			bitmap |= (bitmap << direction_shift * 1);
+			bitmap |= (bitmap >> direction_shift * 1);
 			Console.WriteLine(DebugBitmap(bitmap));
-			bitmap |= (bitmap << direction_shift * 2);
+			bitmap |= (bitmap >> direction_shift * 2);
 			Console.WriteLine(DebugBitmap(bitmap));
-			bitmap |= (bitmap << direction_shift * 4);
+			bitmap |= (bitmap >> direction_shift * 4);
 			Console.WriteLine(DebugBitmap(bitmap));
 			return bitmap;
 		} // end static Fill
@@ -120,9 +228,16 @@ sw    s    se
 			return bitmap;
 		} // end static FillSouth
 		
-		// transforms (3 digit number is a reference to degrees)
+		public static ulong FillSouthAlt(ulong bitmap){
+			bitmap |= (bitmap <<  (-8));
+			bitmap |= (bitmap << (-16));
+			bitmap |= (bitmap << (-32));
+			return bitmap;
+		} // end static FillSouth
 		
-		public static ulong TransformMirror000(ulong bitmap){
+		// transforms //
+		
+		public static ulong TransformMirrorVertical(ulong bitmap){
 			/* 
 			mirror bitmap about the center ranks, along the X axis
 			Parallel prefix approach
@@ -135,9 +250,9 @@ sw    s    se
 			bitmap = ( (bitmap >> 16) & k2) | ( (bitmap & k2) << 16);
 			bitmap = ( (bitmap >> 32)	  )	| ( (bitmap     ) << 32);
 			return bitmap;
-		} // end static TransformMirror000
+		} // end static TransformMirrorVertical
 		
-		public static ulong TransformMirror090(ulong bitmap){
+		public static ulong TransformMirrorHorizontal(ulong bitmap){
 			/* 
 			mirror bitmap about the center files, along the Y axis
 			parallel prefix approach
@@ -150,9 +265,9 @@ sw    s    se
 			bitmap = ( ( bitmap >> 2) & k2) | ( (bitmap & k2) << 2);
 			bitmap = ( ( bitmap >> 4) & k4) | ( (bitmap & k4) << 4);
 			return bitmap;
-		} // end static TransformMirror090
+		} // end static TransformMirrorHorizontal
 		
-		public static ulong TransformMirror045(ulong bitmap){
+		public static ulong TransformMirrorDiagonal(ulong bitmap){
 			/* 
 			mirror bitmap the diagnonal (45 degree axis), a1<->h8
 			*/
@@ -167,9 +282,9 @@ sw    s    se
 			temp    = k1 & (bitmap ^ (bitmap <<  7) );
 			bitmap ^=      (temp   ^ (temp   >>  7) );
 			return bitmap;
-		} // end static TransformMirror045
+		} // end static TransformMirrorDiagonal
 		
-		public static ulong TransformMirror135(ulong bitmap){
+		public static ulong TransformMirrorAntidiagonal(ulong bitmap){
 			/* 
 			mirror bitmap the anti-diagnonal (135 degree axis), a8<->h1
 			*/
@@ -184,21 +299,21 @@ sw    s    se
 			temp    = k1 & (bitmap ^ (bitmap <<  9) );
 			bitmap ^=      (temp   ^ (temp   >>  9) );
 			return bitmap;
-		} // end static TransformMirror135
+		} // end static TransformMirrorAntidiagonal
 		
 		public static ulong TransformRotate180(ulong bitmap){
-			return TransformMirror090( TransformMirror000(bitmap) );
+			return TransformMirrorHorizontal( TransformMirrorVertical(bitmap) );
 		} // end static TransformRotate180
 		
 		public static ulong TransformRotate270(ulong bitmap){
-			return TransformMirror000( TransformMirror045(bitmap) );
+			return TransformMirrorVertical( TransformMirrorDiagonal(bitmap) );
 		} // end static TransformRotate270
 		
 		public static ulong TransformRotate090(ulong bitmap){
-			return TransformMirror045( TransformMirror000(bitmap) );
+			return TransformMirrorDiagonal( TransformMirrorVertical(bitmap) );
 		} // end static TransformRotate090
 		
-		// methods//
+		// methods //
 		
 		public void PieceCreate(int[] piece_type, ulong coordinates){
 			// piece_type[0] is either 0 or 1, indicates white or black
@@ -237,6 +352,7 @@ sw    s    se
 			int piece_iterator = 0;
 			while(piece_iterator < this.pieces.Length){
 				this.pieces[piece_iterator] = 0x0;
+				piece_iterator++;
 			}
 		} // end ClearBoard
 	
--- Engine.cs
+++ Engine.cs
@@ -7,18 +7,43 @@ namespace ZBC{
 		
         public static void Main(string[] args){
 			
+			string[] menu_options = {
+				"coordinate test",
+			};
+			
 			List<Bitboard> history = new List<Bitboard>();
 			history.Add(new Bitboard());
-			
+			string input;
 			
 			while(true){
-				Console.WriteLine(history.ToString());
+				// Menu loop
+				Console.WriteLine("Main Menu");
+				input = Console.ReadLine();
+				
+				if(Array.IndexOf(menu_options, input) < 0){}
+				else if(input.Equals("coordinate test")){ DebugCoordinates(); }
+				else if(input.Equals("")){  }
+				else if(input.Equals("")){  }
+				else if(input.Equals("")){  }
+				else if(input.Equals("")){  }
+				else if(input.Equals("")){  }
+				else if(input.Equals("")){  }
+				else if(input.Equals("")){  }
+				else if(input.Equals("")){  }
+				else if(input.Equals("")){  }
+			}
+			
+			
+				
+				
+				/* Console.WriteLine(history.ToString());
 				Console.WriteLine(history.Count);
 				Console.WriteLine(history[history.Count - 1].ToString());
 				
-				string[] input = Console.ReadLine().Split(' ');
 				int color = 0;
 				int piece = 2;
+				string[] input = Console.ReadLine().Split(' ');
+				
 				if(input[0].Equals("create") && Int32.TryParse(input[1], out color) && Int32.TryParse(input[2], out piece) ){
 					history.Add(history[history.Count - 1].Copy());
 					history[history.Count - 1].PieceCreate(new int[] {color, piece}, Bitboard.CoordinateToUlong(input[3]));
@@ -31,8 +56,12 @@ namespace ZBC{
 					history.Add(history[history.Count - 1].Copy());
 					history[history.Count - 1].PieceMove(Bitboard.CoordinateToUlong(input[1]), Bitboard.CoordinateToUlong(input[2]));
 				}
-				else if(input[0].Equals("dbc")){
-					Console.WriteLine(Bitboard.CoordinateToUlong(input[1]));
+				else if(input[0].Equals("debug")){
+					history.Add(history[history.Count - 1].Copy());
+					ulong temp = history[history.Count - 1].pieces[0] & history[history.Count - 1].pieces[2];
+					temp = Bitboard.FillNorth(temp);
+					history[history.Count - 1].pieces[color] |= temp;
+					history[history.Count - 1].pieces[piece] |= temp;
 				}
 				else if(input[0].Equals("clear")){
 					history.Add(history[history.Count - 1].Copy());
@@ -44,9 +73,54 @@ namespace ZBC{
 				else if(input[0].Equals("end")){
 					break;
 				}
-			}
+				else if(input[0].Equals("fill") && Int32.TryParse(input[1], out color) && Int32.TryParse(input[2], out piece)){
+					history.Add(history[history.Count - 1].Copy());
+					ulong temp = history[history.Count - 1].pieces[color] & history[history.Count - 1].pieces[piece];
+					temp = Bitboard.Fill(temp, input[3]);
+					history[history.Count - 1].pieces[color] |= temp;
+					history[history.Count - 1].pieces[piece] |= temp;
+				}
+			}*/
 
         } // end method Main
+		
+		// Debugging methods
+		
+		public static void DebugCoordinates(){
+			while(true){
+				
+				Console.WriteLine("--------\nCoordinate Debugging Menu. 'exit' to return\n--------");
+				
+				string input = Console.ReadLine();
+				
+				if(input.Equals("exit")){ break; }
+				
+				Console.WriteLine("--------");
+				Console.WriteLine("Coordinate: " + input);
+				
+				int SquareIndex = Bitboard.SquareIndex(input);
+				Console.WriteLine("Square Index: " + SquareIndex.ToString());
+				
+				int FileIndex = Bitboard.FileIndex(SquareIndex);
+				Console.WriteLine("File Index: " + FileIndex.ToString());
+				
+				int RankIndex = Bitboard.RankIndex(SquareIndex);
+				Console.WriteLine("Rank Index: " + RankIndex.ToString());
+				
+				int SquareIndexRecalculated = Bitboard.SquareIndex(FileIndex, RankIndex);
+				Console.WriteLine("Square Index, recalculated: " + SquareIndexRecalculated.ToString());
+				
+				ulong BitmapCoordinate = Bitboard.SquareIndexToBitmap(SquareIndexRecalculated);
+				Console.WriteLine("As a ulong bitmap: " + BitmapCoordinate.ToString());
+				
+				string BitmapVisualized = new string(Bitboard.UlongToVisualizedChars(BitmapCoordinate));
+				Console.WriteLine("Ulong bitmap, visualized: \n");
+				for(int i = 0; i < 8; i++){
+					Console.WriteLine(BitmapVisualized.Substring(i*8, 8));
+				}
+				Console.WriteLine("--------");
+			}
+		}
 		
     } // end class Engine
 } // end namespace ZC
